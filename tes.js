a28e7463f51bcdc9fa21fbb4ebc0f5287a09d660
Vinod Talikoti <35483297+VinodTalikoti@users.noreply.github.com>
2020-08-09 08:13:57 +0530

    Delete tes.js

--- tes.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')({sigint: true});
-const fs = require('fs');
-const dPath= prompt('Enter the path of the folder (ex: C:\\Users\\vinod\\Desktop\\startup_): ');
-
-const dir = dPath + '\\' + 'obfuscate';
-
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
